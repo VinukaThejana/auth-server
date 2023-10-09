e1942c05c21c0405053a14842c5da98e03458b54
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-10-09 20:37:03 +0530

    Added a check migrations feature
    This determines wether or not the schema changes should be migrated to
    the database or not based on user preference and the current enviroment

--- utils/utils.go
+++ utils/utils.go
@@ -1,2 +1,20 @@
 // Package utils contains the utility packages
 package utils
+
+import (
+	"flag"
+	"os"
+
+	"github.com/VinukaThejana/auth/config"
+	"github.com/VinukaThejana/auth/connect"
+)
+
+// CheckForMigrations is a function that checks wether the schema changes should be migrated to the database
+func CheckForMigrations(c *connect.Connector, env *config.Env) {
+	enableMigrations := flag.Bool("migrate", false, "Migrate the schema to the relational database")
+	flag.Parse()
+	if enableMigrations != nil && *enableMigrations {
+		c.MigrateSchemaChanges(env)
+		os.Exit(0)
+	}
+}
