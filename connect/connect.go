7decda91a89c2d041a17c943a1c90737da240da1
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-10-09 18:31:19 +0530

    Added the database connection
    Along with the ability to migrate schema changes in gorm to the
    relational database

--- connect/connect.go
+++ connect/connect.go
@@ -1,2 +1,9 @@
 // Package connect is used to initialize connections to thrid party services
 package connect
+
+import "gorm.io/gorm"
+
+// Connector contains various connections to thrid party serivces
+type Connector struct {
+	DB *gorm.DB
+}
---
+++ connect/db.connect.go
@@ -0,0 +1,47 @@
+package connect
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/VinukaThejana/auth/config"
+	"github.com/VinukaThejana/go-utils/logger"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	gormLogger "gorm.io/gorm/logger"
+)
+
+// InitDatabase is a fucntion to initialize the connection with the postgres database
+func (c *Connector) InitDatabase(env *config.Env) {
+	db, err := gorm.Open(postgres.Open(env.DSN), &gorm.Config{})
+	if err != nil {
+		logger.Errorf(err)
+	}
+
+	if config.GetDevEnv(env) != config.Prod {
+		db.Logger = gormLogger.Default.LogMode(gormLogger.Info)
+	}
+
+	c.DB = db
+}
+
+// MigrateSchemaChanges is a fucntion that is used to migrate schema changes to the database
+func (c *Connector) MigrateSchemaChanges(env *config.Env) {
+	if config.GetDevEnv(env) == config.Prod {
+		logger.Error(fmt.Errorf(" 🪨 Cannot migrate schema changes on production !"))
+		os.Exit(0)
+	}
+
+	migrations := []interface{}{}
+	if len(migrations) == 0 {
+		logger.Error(fmt.Errorf(" ❌ No items to migrate ! "))
+		os.Exit(0)
+	}
+
+	err := c.DB.AutoMigrate(migrations...)
+	if err != nil {
+		logger.Errorf(err)
+	}
+
+	logger.Log("\n\n ✅ All schema changes have been migrated !")
+}
