53ba90558f017a9f6b51fb2aa3f75a18ae8ca587
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-10-12 20:36:22 +0530

    Added needed token schema

---
+++ schemas/token.schema.go
@@ -0,0 +1,15 @@
+package schemas
+
+// RefreshTokenDetails is a function that is used to store refresh token related data
+type RefreshTokenDetails struct {
+	UserID          string
+	AccessTokenUUID string
+}
+
+// RefreshTokenMetadata is a struct that contains details of the device that created the refresh token
+type RefreshTokenMetadata struct {
+	IPAddress string
+	Location  string
+	Device    string
+	OS        string
+}
