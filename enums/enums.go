b0bd00e50e49466ff79bb5399e603f9f8e2fb2b7
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-10-12 13:10:40 +0530

    Added the enums package with heatlh status enums
    This package included all the enums used within the auth backend

---
+++ enums/enums.go
@@ -0,0 +1,9 @@
+// Package enums contains enums
+package enums
+
+const (
+	// SYS_HEALTH -> denotes the health status of the system
+	SYS_HEALTH = "health"
+	// SYS_HEALTH_MSG -> denotes the custom health status message of the system
+	SYS_HEALTH_MSG = "system_message"
+)
