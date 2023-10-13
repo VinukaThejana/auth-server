2b3607cacfd597a5b718e19b4513da6832e8223a
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-10-13 16:32:42 +0530

    Added user related datbase operations

---
+++ services/user.service.go
@@ -0,0 +1,69 @@
+package services
+
+import (
+	"github.com/VinukaThejana/auth/connect"
+	"github.com/VinukaThejana/auth/models"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+// User contains all the user related services
+type User struct {
+	Conn *connect.Connector
+}
+
+// IsEmailAvailable is a function that is used to find out wether the email address is verified or not
+func (u *User) IsEmailAvailable(email string) (
+	userID *uuid.UUID,
+	isEmailAvailable,
+	isEmailVerified bool,
+	err error,
+) {
+	var user models.User
+	err = u.Conn.DB.Select("id", "email", "verified").Where(&models.User{
+		Email: email,
+	}).First(&user).Error
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, true, false, nil
+		}
+
+		return nil, false, false, err
+	}
+
+	return user.ID, false, user.Verified, nil
+}
+
+// IsUsernameAvailable is a function that is used to check wether a username is available
+func (u *User) IsUsernameAvailable(username string) (
+	isUsernameAvailable bool,
+	err error,
+) {
+	var user models.User
+	err = u.Conn.DB.Select("username").Where(&models.User{
+		Username: username,
+	}).First(&user).Error
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return true, nil
+		}
+
+		return false, err
+	}
+
+	return false, nil
+}
+
+// Create is a function that is used to create a new user in the relational database
+func (u *User) Create(user models.User) (
+	newUser models.User,
+	err error,
+) {
+	newUser = user
+	err = u.Conn.DB.Create(&newUser).Error
+	if err != nil {
+		return models.User{}, err
+	}
+
+	return newUser, nil
+}
