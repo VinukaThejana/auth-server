89b295e925bbab71cfc2fb402de15aaaaf8374c9
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-10-23 09:33:55 +0530

    Added user freindly schema

---
+++ schemas/user.schema.go
@@ -0,0 +1,25 @@
+package schemas
+
+import "github.com/VinukaThejana/auth/models"
+
+// User is schema that contians user freindly user details
+type User struct {
+	ID               string `json:"id"`
+	Name             string `json:"name"`
+	Username         string `json:"username"`
+	Email            string `json:"email"`
+	PhotoURL         string `json:"photo_url"`
+	TwoFactorEnabled bool   `json:"two_factor_enabled"`
+}
+
+// FilterUser is a function that is used to filter the user model to a user freindly format
+func FilterUser(user models.User) User {
+	return User{
+		ID:               user.ID.String(),
+		Name:             user.Name,
+		Username:         user.Username,
+		Email:            user.Email,
+		PhotoURL:         user.PhotoURL,
+		TwoFactorEnabled: user.TwoFactorEnabled,
+	}
+}
