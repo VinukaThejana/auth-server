4e8ad6cfe0e6276b0629b0f18d9e700a1ab37987
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-11-04 13:10:58 +0530

    Added a new two factor verification not enabled error

--- errors/errors.go
+++ errors/errors.go
@@ -13,22 +13,23 @@ import (
 //revive:disable
 
 var (
-	ErrInternalServerError       = fmt.Errorf("internal_server_error")
-	ErrUnauthorized              = fmt.Errorf("unauthorized")
-	ErrAccessTokenNotProvided    = fmt.Errorf("access_token_not_provided")
-	ErrBadRequest                = fmt.Errorf("bad_request")
-	ErrIncorrectCredentials      = fmt.Errorf("incorrect_credentials")
-	ErrRefreshTokenExpired       = fmt.Errorf("refresh_token_expired")
-	ErrRefreshTokenNotProvided   = fmt.Errorf("refresh_token_not_provided")
-	ErrAccessTokenExpired        = fmt.Errorf("access_token_expired")
-	ErrUsernameAlreadyUsed       = fmt.Errorf("username_already_used")
-	ErrEmailAlreadyUsed          = fmt.Errorf("email_already_used")
-	ErrEmailConfirmationExpired  = fmt.Errorf("email_confirmation_expired")
-	ErrHaveAnAccountWithTheEmail = fmt.Errorf("already_have_an_account")
-	ErrNoAccountWithEmail        = fmt.Errorf("no_account_with_email")
-	ErrNoAccountWithUsername     = fmt.Errorf("no_account_with_username")
-	ErrAddAUsername              = fmt.Errorf("add_a_username")
-	Okay                         = "okay"
+	ErrInternalServerError             = fmt.Errorf("internal_server_error")
+	ErrUnauthorized                    = fmt.Errorf("unauthorized")
+	ErrAccessTokenNotProvided          = fmt.Errorf("access_token_not_provided")
+	ErrBadRequest                      = fmt.Errorf("bad_request")
+	ErrIncorrectCredentials            = fmt.Errorf("incorrect_credentials")
+	ErrRefreshTokenExpired             = fmt.Errorf("refresh_token_expired")
+	ErrRefreshTokenNotProvided         = fmt.Errorf("refresh_token_not_provided")
+	ErrAccessTokenExpired              = fmt.Errorf("access_token_expired")
+	ErrUsernameAlreadyUsed             = fmt.Errorf("username_already_used")
+	ErrEmailAlreadyUsed                = fmt.Errorf("email_already_used")
+	ErrEmailConfirmationExpired        = fmt.Errorf("email_confirmation_expired")
+	ErrHaveAnAccountWithTheEmail       = fmt.Errorf("already_have_an_account")
+	ErrNoAccountWithEmail              = fmt.Errorf("no_account_with_email")
+	ErrNoAccountWithUsername           = fmt.Errorf("no_account_with_username")
+	ErrAddAUsername                    = fmt.Errorf("add_a_username")
+	ErrTwoFactorverificationNotEnabled = fmt.Errorf("two_factor_verification_not_enabled")
+	Okay                               = "okay"
 )
 
 type res schemas.Res
@@ -93,6 +94,10 @@ func NoAccountWithEmail(c *fiber.Ctx) error {
 
 func NoAccountWithUsername(c *fiber.Ctx) error {
 	return badrequest(c, ErrNoAccountWithUsername)
+}
+
+func TwoFactorVerificationNotEnabled(c *fiber.Ctx) error {
+	return badrequest(c, ErrTwoFactorverificationNotEnabled)
 }
 
 //revive:enable
