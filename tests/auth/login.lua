c174cebc6273a73f5b164ea3af844924dea4ccfc
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-11-04 13:11:27 +0530

    Added the ability to save the access, refresh and session token to a session.json file

--- tests/auth/login.lua
+++ tests/auth/login.lua
@@ -2,6 +2,20 @@ local http = require("socket.http")
 local ltn12 = require("ltn12")
 local json = require("json")
 
+local function getCookie(name, cookies)
+	local function escape(s)
+		return string.gsub(s, "([.*+?%^$(){}|[%]%/\\])", "%%%1")
+	end
+
+	local match = string.match(cookies, "(?:^|;%s*)" .. escape(name) .. "=([^;]*)")
+
+	if match then
+		return match
+	else
+		return nil
+	end
+end
+
 local function help(params)
 	for _, param in ipairs(params) do
 		if param == "-h" or param == "--help" or param == "h" or param == "help" then
@@ -45,7 +59,7 @@ end
 
 local r = {}
 
-local _, code, _ = http.request({
+local _, code, res = http.request({
 	url = "http://localhost:8080/auth/login",
 	method = "POST",
 	headers = headers,
@@ -56,6 +70,64 @@ local _, code, _ = http.request({
 local data = json:decode(r[1])
 
 if code == 200 then
+	local cookies = res["set-cookie"]
+	local accessToken = cookies:match("access_token=([^;]+)")
+	local refreshToken = cookies:match("refresh_token=([^;]+)")
+	local sessionToken = cookies:match("session=([^;]+)")
+
+	local file = io.open("session.json", "w")
+	file:write(json:encode({
+		["__meta__"] = {
+			["about"] = "HTTPie session file",
+			["help"] = "https://httpie.io/docs#sessions",
+			["httpie"] = "3.2.2",
+		},
+		["auth"] = {
+			["password"] = nil,
+			["type"] = nil,
+			["username"] = nil,
+		},
+		["cookies"] = {
+			{
+				["domain"] = "",
+				["expires"] = nil,
+				["name"] = "session",
+				["path"] = "/",
+				["secure"] = false,
+				["value"] = sessionToken,
+			},
+			{
+				["domain"] = "",
+				["expires"] = nil,
+				["name"] = "refresh_token",
+				["path"] = "/",
+				["secure"] = false,
+				["value"] = refreshToken,
+			},
+			{
+				["domain"] = ".localhost",
+				["expires"] = 1698985050,
+				["name"] = "access_token",
+				["path"] = "/",
+				["secure"] = false,
+				["value"] = accessToken,
+			},
+		},
+		["headers"] = {},
+	}))
+	file:close()
+
+	print("")
+	print("COOKIES")
+	print("")
+	print("access : ", accessToken)
+	print("")
+	print("refresh: ", refreshToken)
+	print("")
+	print("session: ", sessionToken)
+	print("")
+	print("")
+
 	print("")
 	print("STATUS")
 	print("")
