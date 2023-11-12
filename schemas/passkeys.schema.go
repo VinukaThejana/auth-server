5b958ba8c1b96905dc138f0e7f168285e7362561
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-11-12 16:43:21 +0530

    Added the passkeys credentials when logging in

--- schemas/passkeys.schema.go
+++ schemas/passkeys.schema.go
@@ -1,7 +1,5 @@
 package schemas
 
-// PassKeyCred struct conatins the passkeys credentials received from the frontend
-type PassKeyCred struct {
 // PassKeyCredWhenCreating struct conatins the passkeys credentials received from the frontend when creating the passkey
 type PassKeyCredWhenCreating struct {
 	ClientExtensionResults  interface{} `json:"clientExtensionResults"`
@@ -13,6 +11,21 @@ type PassKeyCredWhenCreating struct {
 		AttestationObject string        `json:"attestationObject" validate:"required"`
 		ClientDataJSON    string        `json:"clientDataJSON" validate:"required"`
 		Transports        []interface{} `json:"transports"`
+	} `json:"response"`
+}
+
+// PassKeyCredWhenLogginIn struct contians the passkeys credentials received from the frontend while logging in
+type PassKeyCredWhenLogginIn struct {
+	ClientExtensionResults  interface{} `json:"clientExtensionResults"`
+	AuthenticatorAttachment interface{} `json:"authenticatorAttachment"`
+	ID                      string      `json:"id"`
+	Type                    string      `json:"type" validate:"required,oneof=public-key"`
+	RawID                   string      `json:"rawId" validate:"required"`
+	Response                struct {
+		AuthenticatorData string `json:"authenticatorData" validate:"required"`
+		ClientDataJSON    string `json:"clientDataJSON" validate:"required"`
+		Signature         string `json:"signature" validate:"required"`
+		UserHandle        string `json:"userHandle" validate:"required"`
 	} `json:"response"`
 }
 
