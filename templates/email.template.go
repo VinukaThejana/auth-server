1b9a3d3686b50b2fb36df6cb9567e26adb986d4b
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-12-29 14:55:28 +0530

    Revaped the email confirmation template

--- templates/email.template.go
+++ templates/email.template.go
@@ -2,6 +2,7 @@ package templates
 
 import (
 	"bytes"
+	"strings"
 	"text/template"
 )
 
@@ -13,15 +14,81 @@ func (Email) GetEmailConfirmationTmpl(url string) (emailHTML string, err error) 
 	emailVerification := struct{ URL string }{URL: url}
 
 	tmpl := `
-  <h1>Authentication</h1>
-  <br />
-  <strong>Confirm your email address</strong>
-  <br />
-  <a href="{{.URL}}">Click to confirm</a>
-  <br />
-  <br />
-  If you are wondering what is going on please ignore this email
-  `
+<html>
+  <head>
+    <style>
+      .contianer {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        gap: 4;
+        margin-top: 20px;
+        margin-bottom: 40px;
+      }
+      .goto {
+        align-items: center;
+        background-color: #ffffff;
+        border: 1px solid rgba(0, 0, 0, 0.1);
+        border-radius: 0.25rem;
+        box-shadow: rgba(0, 0, 0, 0.02) 0 1px 3px 0;
+        box-sizing: border-box;
+        color: rgba(0, 0, 0, 0.85);
+        cursor: pointer;
+        display: inline-flex;
+        font-family: system-ui, -apple-system, system-ui, "Helvetica Neue",
+          Helvetica, Arial, sans-serif;
+        font-size: 16px;
+        font-weight: 600;
+        justify-content: center;
+        line-height: 1.25;
+        margin: 0;
+        min-height: 3rem;
+        padding: calc(0.875rem - 1px) calc(1.5rem - 1px);
+        position: relative;
+        text-decoration: none;
+        transition: all 250ms;
+        user-select: none;
+        -webkit-user-select: none;
+        touch-action: manipulation;
+        vertical-align: baseline;
+        width: auto;
+      }
+      .goto:hover,
+      .goto:focus {
+        border-color: rgba(0, 0, 0, 0.15);
+        box-shadow: rgba(0, 0, 0, 0.1) 0 4px 12px;
+        color: rgba(0, 0, 0, 0.65);
+      }
+
+      .goto:hover {
+        transform: translateY(-1px);
+      }
+
+      .goto:active {
+        background-color: #f0f0f1;
+        border-color: rgba(0, 0, 0, 0.15);
+        box-shadow: rgba(0, 0, 0, 0.06) 0 2px 4px;
+        color: rgba(0, 0, 0, 0.65);
+        transform: translateY(0);
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Auth Server</h1>
+    <strong>Confirm your email address</strong>
+    <br />
+    <div class="contianer">
+      <section>
+        <a id="goto" class="goto" href="{{.URL}}"> Confirm Email address </a>
+      </section>
+    </div>
+    <footer>
+      If you are wondering about what this email is please ignore this email
+    </footer>
+  </body>
+</html>
+`
 	t := template.Must(template.New("emailVerification").Parse(tmpl))
 
 	var buf bytes.Buffer
