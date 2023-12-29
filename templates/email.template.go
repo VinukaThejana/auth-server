2088c51a3b0d8a4cb622bc543bcbaac39c131b3f
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-12-29 14:56:20 +0530

    Added a template to help reset forgottern passwords

--- templates/email.template.go
+++ templates/email.template.go
@@ -99,3 +99,73 @@ func (Email) GetEmailConfirmationTmpl(url string) (emailHTML string, err error) 
 
 	return buf.String(), nil
 }
+
+// PasswordResetTmpl is a function that is used to get the email with the OTP to reset the password
+func (Email) PasswordResetTmpl(otp string) (emailHTML string, err error) {
+	codes := strings.Split(strings.ReplaceAll(otp, "-", ""), "")
+
+	tmpl := `
+<html>
+  <style>
+    .container {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+      width: 100%;
+      margin-top: 10px;
+      column-gap: 20px;
+    }
+
+    .block {
+      display: flex;
+      border: 2px solid black;
+      border-radius: 20%;
+      width: 50px;
+      height: 50px;
+      align-items: center;
+      justify-content: center;
+    }
+  </style>
+  <h1>Auth Server</h1>
+  <strong> Use the below OTP(One Time Password) to rest your password </strong>
+  <br />
+  <br />
+  <div class="container">
+    <section class="block">{{.CODE1}}</section>
+    <section class="block">{{.CODE2}}</section>
+    <section class="block">{{.CODE3}}</section>
+    <section>
+      <strong>-</strong>
+    </section>
+    <section class="block">{{.CODE4}}</section>
+    <section class="block">{{.CODE5}}</section>
+    <section class="block">{{.CODE6}}</section>
+  </div>
+</html>
+`
+
+	t := template.Must(template.New("resetPassword").Parse(tmpl))
+
+	var buf bytes.Buffer
+	err = t.Execute(&buf, struct {
+		CODE1 string
+		CODE2 string
+		CODE3 string
+		CODE4 string
+		CODE5 string
+		CODE6 string
+	}{
+		CODE1: codes[0],
+		CODE2: codes[1],
+		CODE3: codes[2],
+		CODE4: codes[3],
+		CODE5: codes[4],
+		CODE6: codes[5],
+	})
+	if err != nil {
+		return "", err
+	}
+
+	return buf.String(), nil
+}
